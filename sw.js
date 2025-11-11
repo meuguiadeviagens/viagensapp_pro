5f574ede7ea6edaa446bef7b977807cfecafe5ae
meuguiadeviagens <meuguiadeviagens.app@gmail.com>
2025-11-10 21:30:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Define um nome e versão para o seu cache
 // IMPORTANTE: Mude este nome (ex: v1.0.2) toda vez que atualizar seus arquivos
-const CACHE_NAME = 'meu-guia-de-viagens-v1.1.3';
+const CACHE_NAME = 'meu-guia-de-viagens-v1.1.5';
 
 // Lista de arquivos principais do seu "App Shell"
 const APP_SHELL_URLS = [
@@ -139,3 +139,4 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
