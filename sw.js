154634bc57499cf94dc31110466a04404a52547d
gabrielangst <77883847+gabrielangst@users.noreply.github.com>
2026-06-02 11:36:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Define um nome e versão para o seu cache
 // IMPORTANTE: Mude este nome (ex: v1.0.2) toda vez que atualizar seus arquivos
-const CACHE_NAME = 'meu-guia-de-viagens-v2.0.7';
+const CACHE_NAME = 'meu-guia-de-viagens-v2.0.8';
 
 // Lista de arquivos principais do seu "App Shell"
 const APP_SHELL_URLS = [
